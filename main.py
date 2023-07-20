442468b6fc42c06479a7bbb32222a8d00fdd6f71
Gary Cui <122929865+garycui1@users.noreply.github.com>
2023-07-20 16:03:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import gradio as gr
-def chatbot(conversation):
-    new_message = 'Hello World!'
-    return new_message
-if __name__ == "__main__":
-    with gr.Blocks() as demo:
-        chat = gr.Chatbot()
-        msg = gr.Textbox()
-        clear = gr.ClearButton([msg, chat])
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'genAI FTW!!!!!!'
+
+if __name__ == '__main__':
+    app.run()
+
+
+# def chatbot(conversation):
+#     new_message = 'Hello World!'
+#     return new_message
+# if __name__ == "__main__":
+#     with gr.Blocks() as demo:
+#         chat = gr.Chatbot()
+#         msg = gr.Textbox()
+#         clear = gr.ClearButton([msg, chat])
